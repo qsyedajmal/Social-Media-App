71fcbdbfe26971af471230572a56d59787543cae
Syed Ajmal <qsyedajmal@gmail.com>
2023-06-19 02:17:00 +0530

    topbar

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import "./topbar.css"
+import {Search, Person,Chat,Notifications} from '@mui/icons-material';
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+
+      <div className="topbarLeft">
+        <div className="logo">AJMAL</div>
+      </div>
+
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className='searchIcon' />
+          <input placeholder='Search' className="searchInput" />
+        </div>
+      </div>
+
+      <div className="topbarRight">
+
+        <div className="topbarLinks">
+
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+
+        </div>
+
+          <div className="toppbarIcon">
+
+           <div className="topbarIconItem">
+            <Person/>
+            <span className="topbarIconBadge">1</span>
+           </div>
+
+           <div className="topbarIconItem">
+            <Chat/>
+            <span className="topbarIconBadge">2</span>
+           </div>
+
+           <div className="topbarIconItem">
+            <Notifications/>
+            <span className="topbarIconBadge">3</span>
+           </div>
+
+          </div>
+
+          <img src="/assets/person/1.jpeg" alt='' className="topbarImage" />
+
+        
+
+      </div>
+
+    </div>
+  )
+}
